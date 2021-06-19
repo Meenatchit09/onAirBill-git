709ed9c4be622e38b2c01e8283939a1886b7d646
meenatchit09 <meenatchit09@gmail.com>
2021-06-19 18:23:56 +0530

    Project changes

--- src/UserComponents/Backdrop.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.scss";
-
-const Backdrop = (props) => (
-  <div className="backdrop" onClick={props.onbackDrawerHandler}></div>
-);
-
-export default Backdrop;
